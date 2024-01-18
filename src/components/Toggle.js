05dd3556325b2119f7f37729b0696762da9764c8
Dakota Vikdal <dakota.james19@hotmail.com>
2024-01-18 17:17:03 -0500

    re-learning

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,63 @@
-import React from "react";
+// import React, {useState} from "react";
+
+// function Toggle() {
+//   const [isOn, setIsOn] = useState(false)
+  
+
+//   function toggleButton() {
+//     return(
+//       setIsOn(isOn => !isOn)
+//     )
+//   }
+  
+//   const color = isOn ? "red" : "white"
+
+//   return (
+//     <button 
+//       style = {{background: color}}
+//       onClick={toggleButton}>{isOn ? "ON" : "OFF"}
+//     </button>
+//   )
+// }
+
+// export default Toggle;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React, {useState} from 'react';
+
 
 function Toggle() {
-  return <button>OFF</button>;
+
+  const [isOn, setIsOn] = useState(false)
+
+  function handleClick() {
+    setIsOn(!isOn)
+  } 
+
+  const color = isOn ? "red" : "blue"
+
+
+
+  return (
+    <button style={{ background: color}} onClick={handleClick}>{isOn ? "On" : "Off"}</button>
+  )
 }
 
-export default Toggle;
+
+export default Toggle
